446e6cf551e1572c51db37b5777c36f64032627a
BobIllumine <heredisstultus@gmail.com>
2025-02-26 19:43:58 +0300

    Initial commit

---
+++ xcard-docs/src/pages/react-test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function MyReactPage() {
+  return (
+    <Layout>
+      <h1>My React page</h1>
+      <p>This is a React page</p>
+      <iframe src="https://forms.yandex.ru/cloud/67705de7068ff0657a20c8c0/?iframe=1" frameBorder="0" name="ya-form-67705de7068ff0657a20c8c0" width="650"></iframe> 
+    </Layout>
+  );
+}
---
+++ xcard-docs/src/pages/welcome.md
@@ -0,0 +1,12 @@
+---
+title: Welcome
+---
+
+# Welcome
+
+This is the welcome page.
+
+## Watch a video
+
+<video src="https://www.youtube.com/watch?v=dQw4w9WgXcQ" controls></video>
+
